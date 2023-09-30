0f0818f8f22bfbd9e00a22b83d9d59ad90420e8c
komed3 <pk97@outlook.de>
2023-09-30 10:43:55 +0200

    load express framework

---
+++ server.js
@@ -0,0 +1,17 @@
+/**
+ * periodic-table
+ * Node.js app entry point
+ * 
+ * @author      komed3 (Paul Köhler)
+ * @version     1.0.0
+ */
+
+/**
+ * load "express" web framework
+ */
+const express = require( 'express' );
+
+/**
+ * start web server
+ */
+const app = express();
