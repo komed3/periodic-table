dcbb296889f3470047564fe96bdd0ebd9896546c
komed3 <pk97@outlook.de>
2023-10-01 23:54:06 +0200

    open/close header

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -1,5 +1,12 @@
 jQuery( document ).ready( function ( $ ) {
 
-    //
+    $( document ).on( 'click', '.pt-header-menu', function ( e ) {
+
+        e.preventDefault();
+
+        $( this ).toggleClass( 'active' );
+        $( '.pt-header-nav' ).toggleClass( 'active' );
+
+    } );
 
 } );
