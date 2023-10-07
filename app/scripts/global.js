36dc924fac67f2a42ad8d707f7c8dd76d5f413c4
komed3 <pk97@outlook.de>
2023-10-07 18:10:47 +0200

    change language selector

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -9,6 +9,12 @@ jQuery( document ).ready( function ( $ ) {
 
     } );
 
+    $( document ).on( 'change', '.pt-language-selector-select', function () {
+
+        location.href = '/set?locale=' + $( this ).val();
+
+    } );
+
     $( document ).on( 'click', '.pt-header-menu', function ( e ) {
 
         e.preventDefault();
