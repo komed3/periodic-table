dbb80bec97dc10dddd3a97a2a61c05a75ba7f610
komed3 <pk97@outlook.de>
2023-10-23 15:10:35 +0200

    start reworking code

--- config/routes.js
+++ config/routes.js
@@ -1,26 +1,3 @@
-/**
- * periodic table
- * routing
- */
-
 module.exports.routes = [
-    [ '/', 'start' ],
-    [ '/sitemap/?', 'sitemap' ],
-    [ '/nuclides/?', 'nuclides' ],
-    [ '/spectrum/?', 'spectrum' ],
-    [ '/ionization/?', 'ionization' ],
-    [ '/glossary/?', 'glossary' ],
-    [ '/quiz/?', 'quiz' ],
-    [ '/tools/?', 'tools' ],
-    [ '/abundances/?', 'abundances' ],
-    [ '/abundance/*', 'abundance' ],
-    [ '/element/*', 'element', 'e' ],
-    [ '/e/*', 'element' ],
-    [ '/lists/*', 'lists' ],
-    [ '/list/*', 'list' ],
-    [ '/props/?', 'props' ],
-    [ '/prop/*', 'prop' ],
-    [ '/scale/*', 'scale' ],
-    [ '/search/?*', 'search' ],
-    [ '*', '404' ]
+    [ '/', 'start' ]
 ];
