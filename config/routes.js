2b67b0929f0a5f6cc290856148f0934a240f78c2
komed3 <pk97@outlook.de>
2023-10-28 23:14:50 +0200

    lists page

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ module.exports = [
     [ '/:locale/404/?', '404' ],
     [ '/:locale/element/:element/?', 'element' ],
     [ '/:locale/ionization/?', 'ionization' ],
+    [ '/:locale/lists/?', 'lists' ],
     [ '/:locale/prop/:property/?', 'prop' ],
     [ '/:locale/props/?', 'props' ],
     [ '/:locale/spectrum/?', 'spectrum' ]
