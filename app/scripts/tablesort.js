56930b4c0036ee4aa134b88843c0e41fd8c26c4b
komed3 <pk97@outlook.de>
2023-10-31 11:40:51 +0100

    table sorter

---
+++ app/scripts/tablesort.js
@@ -0,0 +1,107 @@
+window.addEventListener( 'load', function () {
+
+    /**
+     * table sorter
+     * @param {Node} table table to sort
+     * @param {Node} handler sort handler
+     */
+    var ptTableSort = ( table, handler ) => {
+
+        let sortKey = handler.getAttribute( 'sort-key' ),
+            sortDesc = !( ( handler.getAttribute( 'sort-dir' ) || 'desc' ) == 'desc' ),
+            content = table.querySelector( 'tbody' ),
+            items = Array.from( content.querySelectorAll( 'tr' ) );
+
+        /**
+         * sort table items
+         */
+
+        items.sort( ( a, b ) => {
+
+            let valA = parseFloat( a.getAttribute( 'sort-value--' + sortKey ) ),
+                valB = parseFloat( b.getAttribute( 'sort-value--' + sortKey ) );
+
+            return sortDesc
+                ? valB > valA ? 1 : -1
+                : valA > valB ? 1 : -1;
+
+        } );
+
+        /**
+         * rearrange table items 
+         */
+
+        items.forEach( el => {
+
+            content.appendChild( el );
+
+        } );
+
+        /**
+         * update sort handler
+         */
+
+        let header = table.querySelectorAll( 'thead th[sort-key]' );
+
+        [].forEach.call( header, ( th ) => {
+
+            th.classList.remove( 'sort' );
+            th.removeAttribute( 'sort-dir' );
+
+        } );
+
+        handler.classList.add( 'sort' );
+        handler.setAttribute( 'sort-dir', sortDesc ? 'desc' : 'asc' );
+
+    };
+
+    /**
+     * prepare sortable tables
+     */
+
+    var ptSortableTables = document.querySelectorAll( 'table.sortable' );
+
+    [].forEach.call( ptSortableTables, ( table ) => {
+
+        let header = table.querySelectorAll( 'thead th[sort-key]' );
+
+        [].forEach.call( header, ( th ) => {
+
+            /**
+             * create sort handler
+             */
+
+            let handler = document.createElement( 'div' );
+
+            handler.innerHTML = '<span>' + th.innerHTML + '</span><i></i>';
+
+            /**
+             * sort handler event listener
+             */
+
+            handler.addEventListener( 'click', ( e ) => {
+
+                e.preventDefault();
+
+                ptTableSort( table, th );
+
+            }, false );
+
+            th.innerHTML = '';
+            th.appendChild( handler );
+
+            /**
+             * if default sort >> sorting table
+             */
+
+            if( th.classList.contains( 'sort' ) ) {
+
+                ptTableSort( table, th );
+
+            }
+
+        } );
+
+    } );
+
+} );
