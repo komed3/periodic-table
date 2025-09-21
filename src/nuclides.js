c2bd92fdee926aa6d940ec18e7fbb79025b3e5c4
komed3 <pk97@outlook.de>
2025-09-21 18:29:59 +0200

    set upper limit for nuclides grid

--- src/nuclides.js
+++ src/nuclides.js
@@ -5,14 +5,17 @@ const DB = require( './database' );
 const nuclides = ( new DB( 'nuclides' ) ).database;
 const index = ( new DB( 'nuclides_index' ) ).database;
 
+const MAX_Z = 118;
+const MAX_N = 178;
+
 const extractGrid = ( z, n, zEl = 10, nEl = 14 ) => {
 
     z = parseInt( z ) || 0, n = parseInt( n ) || 1;
 
     const minZ = Math.max( 0, z - ( zEl / 2 ) );
-    const maxZ = z + ( zEl - ( z - minZ ) );
+    const maxZ = Math.min( MAX_Z, z + ( zEl - ( z - minZ ) ) );
     const minN = Math.max( 0, n - ( nEl / 2 ) );
-    const maxN = n + ( nEl - ( n - minN ) );
+    const maxN = Math.min( MAX_N, n + ( nEl - ( n - minN ) ) );
 
     const items = [];
 
